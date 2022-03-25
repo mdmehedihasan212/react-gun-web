8833d97fb85b71bd2dda4de4621f0e71aaac9d8c
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-26 00:59:33 +0600

    add header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Products from './components/Products/Products';
+import Header from './components/Header/Header';
 
 function App() {
+
   return (
     <div className="App">
-      <Products></Products>
+      <Header></Header>
     </div>
   );
 }
